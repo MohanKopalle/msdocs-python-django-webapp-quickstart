22af91d206f19f1bde2fc2e0cf1b30abd0fb318a
MohanKopalle <phani.mohan@outlook.com>
2024-05-27 21:30:31 +0530

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CUSTOM_HOSTNAME = 'www.mohankopalle.in'
+#CUSTOM_HOSTNAME = 'www.mohankopalle.in'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,8 +26,8 @@ SECRET_KEY = secrets.token_hex();
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = [WEBSITE_HOSTNAME,CUSTOM_HOSTNAME,]
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
 # Application definition
