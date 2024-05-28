88770c157114f7477f0a764b901ac770beff6d32
MohanKopalle <phani.mohan@outlook.com>
2024-05-28 15:03:09 +0530

    Update production.py

--- quickstartproject/production.py
+++ quickstartproject/production.py
@@ -3,7 +3,7 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-CUSTOM_HOSTNAME='www.mohankopalle.in'
+CUSTOM_HOSTNAME='mohankopalle.in'
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else [CUSTOM_HOSTNAME]
 # ALLOWED_HOSTS = ['www.mohankopalle.in']
 
